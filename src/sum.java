f472249f813746eb1382d0e259d75c7f896e4c2d
Muhibbul Hasan <34686084+muhib95@users.noreply.github.com>
2023-07-22 12:47:21 +0600

    first commit

---
+++ src/sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main (String[] args){
+        int a=10;
+        int b=5;
+        int sum=a+b;
+        System.out.println(sum);
+//        System.out.println(5+5);
+    }
+
+}
